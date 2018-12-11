39425be6cac0bedac30b62def95710037f6dc541
lanchudi <409998481@qq.com>
2018-12-11 19:57:22 +0800

    [Init] Init commit

---
+++ src/main/java/com/chatbot/chatbotconfig/ChatbotConfigApplication.java
@@ -0,0 +1,12 @@
+package com.chatbot.chatbotconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotConfigApplication.class, args);
+    }
+}
---
+++ src/test/java/com/chatbot/chatbotconfig/ChatbotConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chatbot.chatbotconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatbotConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
